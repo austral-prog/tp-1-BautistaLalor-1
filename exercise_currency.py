265573094293a668a8f645c9a0918e9b36e533de
BautistaLalor <blalor@mail.austral.edu.ar>
2026-03-10 11:47:37 -0300

    Add currency conversion from pesos to multiple currencies

--- exercise_currency.py
+++ exercise_currency.py
@@ -11,3 +11,10 @@ def currency():
     tasa_dolar = 1500  # 1 dólar = 1500 pesos
     tasa_euro = 1600   # 1 euro = 1600 pesos
     tasa_real = 250    # 1 real = 250 pesos
+    dolares = pesos / 1500
+    euros = pesos / 1600
+    reales = pesos / 250
+    print(dolares)
+    print(euros)
+    print(reales)
+currency()
